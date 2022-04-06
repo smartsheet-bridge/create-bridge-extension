1190f4531317b87ee3868a60a02d23dfabde7f08
Abha Chandel <abha.chandel@smartsheet.com>
2022-04-06 14:03:34 +0100

    S3 Read for big payload

--- packages/extensions-handler/src/enhancers/handleBigPayLoad.spec.ts
+++ packages/extensions-handler/src/enhancers/handleBigPayLoad.spec.ts
@@ -1,27 +1,70 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { handleBigPayLoad } from './handleBigPayLoad';
 import { createExtensionHandler } from '../handler';
+import { compose } from '../utils/compose';
+import { lambdaTransport } from '../transports/lambdaTransport';
+import { handlePromises } from './handlePromises';
 
-const data = { hello: 'world' };
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const S3_DATA = { s3: 'data' };
+
+const STREAM_PAYLOAD_DATA = { hello: 'world' };
+
+const TEST_PAYLOAD_STREAM = {
+  meta: {
+    type: 'streamExecution',
+  },
+  body: {
+    getUrl: 'google.com',
+    postUrl: 'google.com',
+    payload: { hello: 'world' },
+  },
+};
+const TEST_PAYLOAD_S3 = {
+  meta: {
+    type: 's3Execution',
+  },
+  body: {
+    getUrl: 'google.com',
+    postUrl: 'google.com',
+    payload: { s3: 'data' },
+  },
+};
 
 describe('handleBigPayLoad', () => {
-  it('should call with s3Execution  ', () => {
-    const TEST_LOAD = {
-      meta: {
-        type: 'streamExecution',
-      },
-      body: {
-        getUrl: 'google.com',
-        postUrl: 'google.com',
-        body: '',
-      },
-    };
+  beforeEach(() => {
+    // jest.spyOn(request, 'post').mockResolvedValue({ data: { some: 'dataz' } })
+  });
+  it('should call the next call back from extracted payload from the payload for stream execution  ', () => {
     const cb = jest.fn();
-    const mockget = jest.spyOn(axios, 'post');
-    mockget.mockImplementationOnce(() => Promise.resolve(data));
     const extensibleHandler = createExtensionHandler(handleBigPayLoad);
-    extensibleHandler(TEST_LOAD, cb);
+    extensibleHandler(TEST_PAYLOAD_STREAM, cb);
     expect(cb).toHaveBeenCalledTimes(1);
-    expect(cb).toHaveBeenCalledWith(null, TEST_LOAD);
+    expect(cb).toHaveBeenCalledWith(null, STREAM_PAYLOAD_DATA);
+  });
+  it('should call the next call back payload fetched from S3', () => {
+    const cb = jest.fn();
+    // const mockget = jest.spyOn(axios, 'get');
+    // mockget.mockImplementationOnce(() => Promise.resolve(S3_DATA));
+    //  mockedAxios.get.mockImplementation(() => Promise.resolve(data));
+    // const mockpost = jest.spyOn(axios, 'post');
+    // mockpost.mockImplementationOnce(() => Promise.resolve(S3_DATA));
+    const mockedResponse: AxiosResponse = {
+      data: S3_DATA,
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedResponse);
+    mockedAxios.post.mockResolvedValueOnce(mockedResponse);
+    const extensibleHandler = createExtensionHandler(handleBigPayLoad);
+    extensibleHandler(TEST_PAYLOAD_S3, cb);
+    expect(mockedAxios.get).toHaveBeenCalled();
+    expect(mockedAxios.post).toHaveBeenCalled();
+    expect(cb).toHaveBeenCalledTimes(1);
+    expect(cb).toHaveBeenCalledWith(null, S3_DATA);
   });
 });
--- packages/extensions-handler/src/enhancers/handleBigPayLoad.ts
+++ packages/extensions-handler/src/enhancers/handleBigPayLoad.ts
@@ -4,45 +4,73 @@ import axios from 'axios';
 import { normalizeError } from '../utils/normalizeError';
 import { ExtensionHandlerEnhancer } from '../handler';
 
-exports.S3_EXECUTION = 's3Execution';
-const isS3Execution = payload => payload.meta.type === exports.S3_EXECUTION;
+const S3_EXECUTION = 's3Execution';
+const STREAM_EXECUTION = 'streamExecution';
+
+const getPayloadFromS3 = async (url: string) => {
+  const resp = await axios.get(url);
+  return resp.data(); // (Or whatever)
+};
+
+const putPayloadToS3 = async (url: string, payload: any) => {
+  const headers = new Headers({ 'Content-Type': 'application/json' });
+  const resp = await axios.post(url, {
+    method: 'POST',
+    headers,
+    body: payload,
+  });
+  return resp.data(); // (Or whatever)
+};
+
+type S3Execution = {
+  meta: {
+    type: typeof S3_EXECUTION;
+  };
+  body: {
+    getUrl: string;
+    postUrl: string;
+    payload: unknown;
+  };
+};
+type StreamExecution = {
+  meta: {
+    type: typeof STREAM_EXECUTION;
+  };
+  body: {
+    getUrl: string;
+    postUrl: string;
+    payload: unknown;
+  };
+};
+const isStreamExecution = (payload: any): payload is StreamExecution =>
+  payload.meta.type === STREAM_EXECUTION;
+
+const isS3Execution = (payload: any): payload is S3Execution =>
+  payload.meta.type === S3_EXECUTION;
 
 export const handleBigPayLoad: ExtensionHandlerEnhancer = create => () => {
   const handler = create();
   return (payload, callback) => {
-    let payloadNext;
     if (isS3Execution(payload)) {
-      axios
-        .get(payload.body.getUrl)
-        .then(response => {
-          payloadNext = response.data;
+      getPayloadFromS3(payload.body.getUrl)
+        .then(payloadFronmS3 => {
+          handler(payloadFronmS3, (err, result: any) => {
+            putPayloadToS3(payload.body.postUrl, result)
+              .then(s3Result => {
+                callback(err, s3Result);
+              })
+              .catch(error => {
+                return normalizeError(error);
+              });
+          });
         })
         .catch(err => {
           return normalizeError(err);
         });
-    } else {
-      payloadNext = payload.body.payload;
+    } else if (isStreamExecution(payload)) {
+      handler(payload, (err, result: any) => {
+        callback(err, result);
+      });
     }
-    handler({ payload, payloadNext }, (err, result: any) => {
-      let resultSend;
-      if (isS3Execution(payload)) {
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        axios
-          .post(payload.body.postUrl, {
-            method: 'POST',
-            headers,
-            body: result,
-          })
-          .then(response => {
-            resultSend = response.data;
-          })
-          .catch(error => {
-            return normalizeError(error);
-          });
-      } else {
-        resultSend = result;
-      }
-      callback(err, resultSend);
-    });
   };
 };

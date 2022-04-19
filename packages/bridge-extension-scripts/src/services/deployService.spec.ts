1f9c63c485ce424f7530d8d01634bcb21b2b7350
Pratik Bhonsle <pratik.bhonsle@smartsheet.com>
2022-04-19 04:58:51 +0100

    test: unittests for build, WIP for deploy

--- packages/bridge-extension-scripts/src/services/deployService.spec.ts
+++ packages/bridge-extension-scripts/src/services/deployService.spec.ts
@@ -1,6 +1,28 @@
+import mockFs from 'mock-fs';
+import { createBridgeService } from './bridgeService';
 import { createDeployService } from './deployService';
 
-describe('deployService', () => {
+jest.mock('./bridgeService', () => {
+  const originalModule = jest.requireActual('./bridgeService');
+  return {
+    __esModule: true,
+    ...originalModule,
+    createBridgeService: jest.fn((host, auth) => ({
+      instance: { host, auth },
+      extension: { uploadSpec: jest.fn() },
+      platform: { get: jest.fn() },
+    })),
+  };
+});
+
+describe('createDeployService', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
   it('does not break', () => {
     expect(() =>
       createDeployService({
@@ -8,12 +30,77 @@ describe('deployService', () => {
         auth: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxx-xxxxxx',
         out: 'lib',
         options: {
-          // exclude: [],
-          // include: '**/**',
           symlinks: false,
           specFile: 'extension.json',
         },
       })
     ).not.toThrow();
   });
+
+  it('creates the sdk with right options', () => {
+    createDeployService({
+      host: 'https://extension.example.com',
+      auth: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxx-xxxxxx',
+      out: 'lib',
+      options: {},
+    });
+
+    expect(createBridgeService).toBeCalledWith(
+      'https://extension.example.com',
+      'xxxxxxxx-xxxx-xxxx-xxxx-xxxxx-xxxxxx'
+    );
+  });
+});
+
+describe('deploy', () => {
+  beforeEach(() => {
+    mockFs({
+      'extension.json': JSON.stringify({
+        name: 'ext1',
+        version: '1.0.0',
+      }),
+      'package.json': '{}',
+    });
+  });
+  afterEach(() => {
+    mockFs.restore();
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it.skip('authenticates by calling sdk.platform.get()', async () => {
+    const sut = createDeployService({
+      host: 'https://extension.example.com',
+      auth: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxx-xxxxxx',
+      out: 'lib',
+      options: {
+        specFile: 'extension.json',
+      },
+    });
+
+    await expect(
+      sut().then(() => {
+        expect(
+          createBridgeService(
+            'https://extension.example.com',
+            'xxxxxxxx-xxxx-xxxx-xxxx-xxxxx-xxxxxx'
+          ).platform.get
+        ).toBeCalledTimes(1);
+      })
+      // .catch(e => console.log(e))
+    ).resolves.not.toThrow();
+  });
+
+  it.todo('creates an instance of archiver with right options');
+  it.todo('calls archiver.glob with right options');
+  it.todo('throws if archiver raises warning');
+  it.todo('throws if archiver raises error');
+
+  it.todo('uploads spec by calling sdk.extension.uploadSpec()');
+  it.todo('uploads by calling sdk.instance() to uploadTo destination');
+  it.todo('skips upload if uploadTo absent from response');
+
+  it.todo('activates revision by calling sdk.extension.activateRevision()');
 });
